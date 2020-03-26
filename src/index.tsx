aa1a7ea2842bb78fe9f1b0cf27f13a2a415ffb2d
SmartFox <alexa@MacBook-Air-Aleksandra.local>
2020-03-26 19:50:34 +0300

    Tabs and icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import * as serviceWorker from './serviceWorker';
+import {defineCustomElements} from '@ionic/pwa-elements/loader';
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-serviceWorker.unregister();
